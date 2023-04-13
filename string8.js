7ec77e4f5d1173bb39056b84b9d2ae46ef254e7d
shacz007 <sachinsiddrampur@gmail.com>
2023-04-13 10:44:06 +0530

    6,7,8

---
+++ string8.js
@@ -0,0 +1,9 @@
+// replace()
+// replaces by taking two parameters a word to be replaced 
+// and a word with whcih it has to be replaced
+
+let a = "sakugar tama";
+
+let b = a.replace("tama","mata");
+
+console.log(b);
