a51e6570fee687e21d56163f567d0d98bfc29e1d
shacz007 <sachinsiddrampur@gmail.com>
2023-04-13 11:50:00 +0530

    11 12

---
+++ string11.js
@@ -0,0 +1,13 @@
+// trim()
+
+// The trim() method removes whitespace from both sides of a string
+
+let a = "                     -- magical --                    "
+let b = function trim(){
+    return a.trim();
+}
+console.log(a);
+console.log(b());
+
+
+// there are trimstart and trimend method also  
---
+++ string12.js
@@ -0,0 +1,7 @@
+// charAt()
+// character at particular index
+
+let a = " missssipppo"
+
+let b = a.charAt(2);
+console.log(b);
