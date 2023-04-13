2ef2471272eea771ef4ced95e4b65404801c3962
shacz007 <sachinsiddrampur@gmail.com>
2023-04-13 10:47:54 +0530

    9

--- string8.js
+++ string8.js
@@ -1,6 +1,7 @@
 // replace()
 // replaces by taking two parameters a word to be replaced 
 // and a word with whcih it has to be replaced
+// replaces only first instaces,,,,,to replace all use replaceAll()
 
 let a = "sakugar tama";
 
---
+++ string9.js
@@ -0,0 +1,7 @@
+// replace all()
+// replaces all instances where ever occured throughout
+
+let a = "mayor roll, pay man, roll costa, roll singals";
+let b = a.replaceAll("roll", "call");
+
+ console.log(b);
