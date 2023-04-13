e63ab7f03307a17dafe899ed2dba35db6d091ed9
shacz007 <sachinsiddrampur@gmail.com>
2023-04-13 10:38:27 +0530

    5

---
+++ string5.js
@@ -0,0 +1,8 @@
+// split the string 
+// splits into characters in case of single word
+// splits into words in case of sentences
+
+let a =  "martin Luther"
+let b = a.split(" ");
+
+console.log(b);
